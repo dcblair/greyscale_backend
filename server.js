8cb9d4182d9d2a3ca455466d7e6608f10c588e3a
dcblair <devin.c.blair@gmail.com>
2021-01-06 18:40:23 -0800

    edit cors origin

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://greyscale-music.firebaseapp.com/'],
+  origin: ['http://localhost:3000', 'https://greyscale-music.web.app/'],
   credentials: true,
   optionsSuccessStatus: 204
 }
